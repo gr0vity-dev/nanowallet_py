d528475b2589a1b4385eb9dc1c633883c7cc275c
gr0vity-dev <85646666+gr0vity-dev@users.noreply.github.com>
2025-04-21 18:09:04 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="nanowallet",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "nano_lib_py==0.5.1",
